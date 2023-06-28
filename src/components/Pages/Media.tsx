8f79cf8863d02054b7424be30d5040019b67936d
gabjuh <jjjjgabesz@gmail.com>
2023-06-28 11:18:41 +0200

    create media component

---
+++ src/components/Pages/Media.tsx
@@ -0,0 +1,15 @@
+import ImageSlider from "./ImageSlider";
+import Videos from "./Videos";
+import Audio from "./Audio";
+
+const Media = () => {
+  return (
+    <>
+      <ImageSlider />
+      <Audio />
+      <Videos />
+    </>
+  );
+};
+
+export default Media;
